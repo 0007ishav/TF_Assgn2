e0a389c4f79aa21f09955f682977f13d571ee32c
Ishav <106452155+0007ishav@users.noreply.github.com>
2023-10-22 16:42:54 +0530

    Update main.tf

--- main.tf
+++ main.tf
@@ -4,11 +4,18 @@ resource "aws_instance" "myInstance" {
   ami = var.ami_value
   instance_type = var.ins_type
 
- tags = var.tags
-
-  volume_tags = var.volume_tags
-}
-
+ tags = merge(
+      var.tags,
+      {
+        Name = each.key
+      }
+)
+  volume_tags = merge(
+      var.volume_tags,
+      {
+        Name = each.key
+      }
+)
 resource "aws_iam_user" "ishav_iam" {
   count = length(var.Name)
   name = var.Name[count.index]
